20be1a37560344aa383767b65208a5890eb8fc58
BobbieO <bobbie2504@msn.com>
2016-06-13 13:35:09 -0500

    Added divs and rows to page for future foreach loop.

--- app/views/clients.blade.php
+++ app/views/clients.blade.php
@@ -1,14 +1,28 @@
 @extends('layouts.master')
 
 @section('title')
-    Flit My Clients
+    Flit: My Clients
 @stop
 
 @section('content')
 <main>
 
+    <div class="container">
+        <div>
+            <h1>All Clients</h1>
+        </div>
 
-
+    <!--foreach loop here to propagate data, will also need paginate/row thing prob  -->
+        <div class="row">
+            <div class="col s3">
+                <a href="#">Client Name Here</a>
+                <p>Contact Info</p>
+                <p>Project Name</p>
+                <p>Project Details</p>
+            </div> 
+        </div> <!-- closes row -->  
+    </div> <!-- closes container -->
+       
 
 </main>
 @stop
