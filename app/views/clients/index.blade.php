b46e7abcb9e721266fef2134c2a62e96849d8375
BobbieO <bobbie2504@msn.com>
2016-06-13 17:29:53 -0500

    Added foreach loop for all-clients page to display all clients

--- app/views/clients/index.blade.php
+++ app/views/clients/index.blade.php
@@ -13,16 +13,18 @@
         </div>
 
     <!--foreach loop here to propagate data, will also need paginate/row thing prob  -->
-        <div class="row">
-            <div class="col s3">
-                <a href="#">Client Name Here</a>
-                <p>Contact Info</p>
-                <p>Project Name</p>
-                <p>Project Details</p>
-            </div> 
-        </div> <!-- closes row -->  
+        @foreach($clients as $client)
+                <div class="row">
+                    <div class="col s3">
+                        <h3><a href="{{{ action('ClientsController@show', $client->id) }}}">Client: {{{$client->client_name}}}</a></h3>
+                        <p>Main Contact: {{{$client->main_poc_name}}}</p>
+                        <p>Main Contact Phone: {{{$client->main_poc_phone}}}</p>
+                        <p>Main Contact Email: {{{$client->main_poc_email}}}</p>
+                        <p>Main Contact Address: {{{$client->main_poc_address}}}</p> 
+                    </div> 
+                </div> <!-- closes row -->
+        @endforeach
     </div> <!-- closes container -->
        
-
 </main>
 @stop
