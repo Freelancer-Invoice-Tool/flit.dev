c3b1c70c5f61d7b27e1b42e7b854c177de8ec83d
BobbieO <bobbie2504@msn.com>
2016-06-13 15:38:43 -0500

    Added table, placeholder table data row

--- app/views/dashboard.blade.php
+++ app/views/dashboard.blade.php
@@ -1,48 +1,69 @@
 @extends('layouts.master')
 
 @section('title')
-    Flit: My Projects
+    Flit: Dashboard
 @stop
 
 @section('content')
 <main>
     <div class="container">
-
-        <div class="row">
-            <div class="col s6">
-                <!-- Dropdown Triggers -->
-                <a class='dropdown-button btn' href='#' data-activates='dropClients'>Contacts</a>
+        <div>
+            <div class="row">
+                <div class="col s6">
+                    <!-- Dropdown Triggers -->
+                    <a class='dropdown-button btn' href='#' data-activates='dropClients'>Contacts</a>
+                </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col s6">
-                <a class='dropdown-button btn' href='#' data-activates='dropDates'>Deadlines</a>
+            <div class="row">
+                <div class="col s6">
+                    <a class='dropdown-button btn' href='#' data-activates='dropDates'>Deadlines</a>
+                </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col s6">
-                <a class='dropdown-button btn' href='#' data-activates='dropPay'>Pay Days</a>
+            <div class="row">
+                <div class="col s6">
+                    <a class='dropdown-button btn' href='#' data-activates='dropPay'>Pay Days</a>
+                </div>
             </div>
+
+            <!-- Dropdown Structures -->
+            <ul id='dropClients' class='dropdown-content'>
+                <li><a href="#!">one</a></li>
+                <li><a href="#!">two</a></li>
+                <li><a href="#!">three</a></li>
+            </ul>
+
+            <ul id='dropDates' class='dropdown-content'>
+                <li><a href="#!">one</a></li>
+                <li><a href="#!">two</a></li>
+                <li><a href="#!">three</a></li>
+            </ul>
+
+            <ul id='dropPay' class='dropdown-content'>
+                <li><a href="#!">one</a></li>
+                <li><a href="#!">two</a></li>
+                <li><a href="#!">three</a></li>
+            </ul>
         </div>
 
-        <!-- Dropdown Structures -->
-        <ul id='dropClients' class='dropdown-content'>
-            <li><a href="#!">one</a></li>
-            <li><a href="#!">two</a></li>
-            <li><a href="#!">three</a></li>
-        </ul>
+        <div>
+            <table class="striped centered responsive-table">
+                <thead>
+                    <tr>
+                        <th data-field="project">Project</th>
+                        <th data-field="deadline">Deadline</th>
+                        <th data-field="details" class="truncate">Details</th>
+                    </tr>
+                </thead>
 
-        <ul id='dropDates' class='dropdown-content'>
-            <li><a href="#!">one</a></li>
-            <li><a href="#!">two</a></li>
-            <li><a href="#!">three</a></li>
-        </ul>
-
-        <ul id='dropPay' class='dropdown-content'>
-            <li><a href="#!">one</a></li>
-            <li><a href="#!">two</a></li>
-            <li><a href="#!">three</a></li>
-        </ul>
+                <tbody>
+                    <tr>
+                        <td>Project X</td>
+                        <td>March</td>
+                        <td>Blahblahblahblahblahblahblah</td>
+                    </tr>
+                </tbody>
+          </table>
+        </div>
     </div>
 </main>
 @stop
