15d89c78f85a32090943bdf94d313a8152ed4a58
BobbieO <bobbie2504@msn.com>
2016-06-13 15:24:49 -0500

    Added dropdown menus to body for database info

--- app/views/dashboard.blade.php
+++ app/views/dashboard.blade.php
@@ -6,10 +6,43 @@
 
 @section('content')
 <main>
-
     <div class="container">
 
+        <div class="row">
+            <div class="col s6">
+                <!-- Dropdown Triggers -->
+                <a class='dropdown-button btn' href='#' data-activates='dropClients'>Contacts</a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col s6">
+                <a class='dropdown-button btn' href='#' data-activates='dropDates'>Deadlines</a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col s6">
+                <a class='dropdown-button btn' href='#' data-activates='dropPay'>Pay Days</a>
+            </div>
+        </div>
 
+        <!-- Dropdown Structures -->
+        <ul id='dropClients' class='dropdown-content'>
+            <li><a href="#!">one</a></li>
+            <li><a href="#!">two</a></li>
+            <li><a href="#!">three</a></li>
+        </ul>
+
+        <ul id='dropDates' class='dropdown-content'>
+            <li><a href="#!">one</a></li>
+            <li><a href="#!">two</a></li>
+            <li><a href="#!">three</a></li>
+        </ul>
+
+        <ul id='dropPay' class='dropdown-content'>
+            <li><a href="#!">one</a></li>
+            <li><a href="#!">two</a></li>
+            <li><a href="#!">three</a></li>
+        </ul>
     </div>
 </main>
 @stop
