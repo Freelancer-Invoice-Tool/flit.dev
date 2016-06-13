d467017ad3428bd0276d4b610f12789dabb34df2
BobbieO <bobbie2504@msn.com>
2016-06-13 11:34:00 -0500

    Entered placeholder for signup link and form action.

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -1,13 +1,51 @@
 @extends('layouts.master')
 
 @section('title')
-    Flit
+    Flit Home Login
 @stop
 
 @section('content')
 <main>
-    <div class="container">
+    <div class="container"> 
+        <div class="row">
+            <div class="col s12">
+                <h2>Log In</h2>
+            </div>
+        </div>
 
+        <div class="row">
+            <div class="col s8">
+                <form class="col s12" method="POST" enctype="multipart/form-data" action="{#">
+                    {{Form::token()}}
+
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <input name="username" id="username" type="text" class="validate">
+                            <label for="username">Username</label>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <input name="password" id="password" type="password" class="validate">
+                            <label for="password">Password</label>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col s12">
+                            <button type="submit" class="btn waves-effect waves-light">Log In</button>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col s6"
+                            <p>Don't have an account?</p>
+                        </div>
+                        <div class="col s6">
+                            <a href="#">Sign Up</a>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
 </main>
 @stop
