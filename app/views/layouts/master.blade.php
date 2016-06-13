8f989b479ea8a19c1d4f9538346e56a28eececd5
BobbieO <bobbie2504@msn.com>
2016-06-13 10:50:36 -0500

    Moved file to layouts folder. Added title yield, content yield, materialize icons link, css link, and footer.

---
+++ app/views/layouts/master.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+
+    <title>@yield('title')</title>
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+    <!-- Compiled and minified materialize CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css">
+
+    <!-- materialize icons -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <!-- our css -->
+    <link rel="stylesheet" type="text/css" href="#">
+          
+</head>
+
+<body>
+
+    @yield('content')
+
+
+
+    <footer>
+        <div>
+            <p>&#169; 2016</p>
+        </div>
+    </footer>
+
+<!-- Compiled and minified JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
+</body>
+</html>
