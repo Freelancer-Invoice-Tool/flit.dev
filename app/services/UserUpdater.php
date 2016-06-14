996db861aec572a8016040d4789d7cc813aed37a
Alan <alaurit@gmail.com>
2016-06-14 15:23:35 -0500

    fixed user edit functionality, need to change password requirement when processing form

--- app/services/UserUpdater.php
+++ app/services/UserUpdater.php
@@ -1,12 +1,11 @@
 <?php 
 
 class UserUpdater {
-    public function updateclient(Illuminate\Http\Request $request, User $user) {
-        $user->email = $request->input('email');
+    public function updateUser(Illuminate\Http\Request $request, User $user) {
         $user->first_name = $request->input('first_name');
         $user->last_name = $request->input('last_name');
         $user->email = $request->input('email');
-        $user->password = $request->input("$_ENV['USER_PASSWORD']");
+        $user->password = $request->input('password');
 
         if ($user->save() === false) {
             throw new RecordNotSavedException("User failed to save, this should never happen. Investigate!");
