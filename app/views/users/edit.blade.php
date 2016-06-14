97f3462cec95b1420c0b056fbbb2ad7118fc6bdf
Alan <alaurit@gmail.com>
2016-06-14 14:55:13 -0500

    changed Edit page to laravel form, added CSRF requirement to BaseController

--- app/views/users/edit.blade.php
+++ app/views/users/edit.blade.php
@@ -9,46 +9,51 @@
 <main>
 
     <div class="container">
-        <div class="row">
-            <div class="col s8">
-                <h1>Edit Your Account</h1>
+        <!-- model form automatically fills in user info as placeholders -->
+        {{ Form::model($user, array('action' => array('UserController@update', $user->id), 'method' => 'PUT'))}}
+            <div class="row">
+                <div class="col s8">
+                    <h1>Edit Your Account</h1>
+                </div>
             </div>
-        </div>
 
-        <div class="col s6">
-             <div class="row">
-                <form class="col s12">
-                    <div class="row">
-                        <div class="input-field col s6">
-                            <input placeholder="Placeholder" id="first_name" type="text" class="validate">
-                            <label for="first_name">First Name</label>
+            <div class="col s6">
+                 <div class="row">
+                    <form class="col s12">
+                        <div class="row">
+                            <div class="input-field col s6">
+                                {{Form::text('first_name', null, array('class'=>'validate'))}}
+                                {{ Form::label('first_name', 'First Name') }}
+                            </div>
+                            <div class="input-field col s6">
+                                {{ Form::text('last_name', null, array('class'=>'validate')) }}
+                                {{ Form::label('last_name', 'Last Name') }}
+                            </div>
                         </div>
-                        <div class="input-field col s6">
-                            <input id="last_name" type="text" class="validate">
-                            <label for="last_name">Last Name</label>
-                        </div>
-                    </div>
 
-                    <div class="row">
-                        <div class="input-field col s12">
-                            <input id="email" type="email" class="validate">
-                            <label for="email">Email</label>
+                        <div class="row">
+                            <div class="input-field col s12">
+                                {{ Form::email('email', null, array('class'=>'validate')) }}
+                                {{ Form::label('email', 'Email') }}
+                            </div>
                         </div>
-                    </div>
 
-                    <div class="row">
-                        <div class="input-field col s12">
-                            <input id="password" type="password" class="validate">
-                            <label for="password">Password</label>
+                        <div class="row">
+                            <div class="input-field col s12">
+                                {{ Form::password('password', null, array('class'=>'validate')) }}
+                                <!-- <input id="password" type="password" class="validate"> -->
+                                {{ Form::label('password', 'Password') }}
+                                <!-- <label for="password">Password</label> -->
+                            </div>
                         </div>
-                    </div>
 
-                    <div>
-                        <button type="submit" class="btn">Save New Info?</button>     
-                    </div>
-                </form>
+                        <div>
+                            <button type="submit" class="btn">Save New Info?</button>     
+                        </div>
+                    </form>
+                </div>
             </div>
-        </div>
+        {{ Form::close() }}
     </div>
 </main>
 @stop
