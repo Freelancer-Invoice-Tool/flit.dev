639f835e6354b2b8c1bd3e31caf6bc39b42b7cfe
BobbieO <bobbie2504@msn.com>
2016-06-14 13:39:26 -0500

    Edited form labels for clarity

--- app/views/projects/edit.blade.php
+++ app/views/projects/edit.blade.php
@@ -38,18 +38,18 @@
             
                 <div class="input-field col s6">
                     {{ Form::text('project_submitted_date', null, array('id'=>'project_submitted_date')) }}
-                    {{ Form::label('project_submitted_date', 'Project Submitted') }}
+                    {{ Form::label('project_submitted_date', 'Date Project Submitted') }}
                 </div>
             </div>
             <div class="row">
                 <div class="input-field col s6">
                     {{ Form::text('invoice_submitted_date', null, array('id'=>'invoice_submitted_date')) }}
-                    {{ Form::label('invoice_submitted_date', 'Invoice Submitted') }}
+                    {{ Form::label('invoice_submitted_date', 'Date Invoice Submitted') }}
                 </div>
             
                 <div class="input-field col s6">
                     {{ Form::text('invoice_approval_date', null, array('id'=>'invoice_approval_date')) }}
-                    {{ Form::label('invoice_approval_date', 'Invoice Approved') }}
+                    {{ Form::label('invoice_approval_date', 'Date Invoice Approved') }}
                 </div>
             </div>
             <div class="row">
@@ -60,30 +60,30 @@
             
                 <div class="input-field col s6">
                     {{ Form::text('payment_received', null, array('id'=>'payment_received')) }}
-                    {{ Form::label('payment_received', 'Payment Received') }}
+                    {{ Form::label('payment_received', 'Date Payment Received') }}
                 </div>
             </div>
             <div class="row">
                 <div class="input-field col s6">
                     {{ Form::text('project_poc_name', null, array('id'=>'project_poc_name')) }}
-                    {{ Form::label('project_poc_name', 'Point of Contact Name') }}
+                    {{ Form::label('project_poc_name', 'Project Contact\'s Name') }}
                 </div>
            
                 <div class="input-field col s6">
                     {{ Form::text('project_poc_phone', null, array('id'=>'project_poc_phone')) }}
-                    {{ Form::label('project_poc_phone', 'Point of Contact Phone') }}
+                    {{ Form::label('project_poc_phone', 'Project Contact\'s Phone') }}
                 </div>
             </div>
             <div class="row">
                 <div class="input-field col s12">
                     {{ Form::text('project_poc_email', null, array('id'=>'project_poc_email')) }}
-                    {{ Form::label('project_poc_email', 'Point of Contact Email') }}
+                    {{ Form::label('project_poc_email', 'Project Contact\'s Email') }}
                 </div>
             </div>
             <div class="row">
                 <div class="input-field col s12">
                     {{ Form::text('project_poc_address', null, array('id'=>'project_poc_address')) }}
-                    {{ Form::label('project_poc_address', 'Point of Contact Address') }}
+                    {{ Form::label('project_poc_address', 'Project Contact\'s Address') }}
                 </div>
             </div>
 
