2feb5a7ebcfa461207b89526255ba6971bae019c
kcates81 <kriscates81@gmail.com>
2016-06-14 10:30:42 -0500

    finished create client page

--- app/views/clients/create.blade.php
+++ app/views/clients/create.blade.php
@@ -16,9 +16,27 @@
         {{Form::open(array('action' => 'ClientsController@store'))}} 
             <div>
                 {{Form::label('client_name', 'Client Name')}} 
-                {{Form::text('title', Input::old('title'), array('class' => 'form-control other-class another', 'placeholder' => 'Client Name'))}} 
+                {{Form::text('title', Input::old('title'), array('class' => 'form-control other-class another', 'placeholder' => 'e.g. ABC Company, Inc.'))}} 
             </div>
-
+            <div>
+                {{Form::label('main_poc_name', 'Main Point of Contact Name')}}
+                {{Form::text('main_poc_name', Input::old('main_poc_name', array('class' => 'form-control other-class another', 'placeholder' => 'e.g. Joe Blow')))}}
+            </div>
+            <div>
+                {{Form::label('main_poc_email', 'Main Point of Contact Email')}}
+                {{Form::text('main_poc_email', Input::old('main_poc_email'), array('class' => 'form-control other-class another', 'placeholder' => 'e.g. jblow@example.com'))}}
+            </div>
+            <div>
+                {{Form::label('main_poc_phone', 'Main Point of Contact Phone')}}
+                {{Form::text('main_poc_phone', Input::old('main_poc_phone'), array('class' => 'form-control other-class another', 'placeholder' => 'e.g. 210-867-5309'))}}
+            </div>
+            <div>
+                {{Form::label('main_poc_address', 'Main Point of Contact Address')}}
+                {{Form::text('main_poc_address', Input::old('main_poc_address'), array('class' => 'form-control other-class another', 'placeholder' => 'e.g. 123 Some Street Anytown, TX 78253'))}}
+            </div>
+            <div>
+                <button type="submit" class="btn">Create Client</button>     
+            </div>
         {{Form::close()}}
     </div>
 @stop
