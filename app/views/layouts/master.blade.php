851c54990c60f73863ac96f453bb7599dc43030f
BobbieO <bobbie2504@msn.com>
2016-06-16 11:34:21 -0500

    Changed font throughout.

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -9,6 +9,9 @@
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
+    <!-- google font -->
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,400italic,700' rel='stylesheet' type='text/css'>
+
     <!-- Compiled and minified materialize CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css">
 
