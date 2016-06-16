22164b61e803d039396e4179b28a80f9f3c0eb90
kcates81 <kriscates81@gmail.com>
2016-06-16 15:38:51 -0500

    add columns to updater

--- app/services/ProjectUpdater.php
+++ app/services/ProjectUpdater.php
@@ -6,11 +6,15 @@ class ProjectUpdater {
         $project->client_id = $client->id;
         $project->name = $request->input('name');
         $project->description = $request->input('description');
+        $project->project_notes = $request->input('project_notes');
         $project->due_date = $request->input('due_date');
         $project->project_submitted_date = $request->input('project_submitted_date');
         $project->invoice_submitted_date = $request->input('invoice_submitted_date');
         $project->invoice_approval_date = $request->input('invoice_approval_date');
         $project->pay_date = $request->input('pay_date');
+        $project->project_status = $request->input('project_status');
+        $project->project_submitted = $request->input('project_submitted');
+        $project->invoice_submitted = $request->input('invoice_submitted');
         $project->payment_received = $request->input('payment_received');
         $project->project_poc_name = $request->input('project_poc_name');
         $project->project_poc_email = $request->input('project_poc_email');
