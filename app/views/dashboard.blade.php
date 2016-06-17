28d46de335c4dd89ca5e737d189179ce6ad30ca2
BobbieO <bobbie2504@msn.com>
2016-06-17 15:22:05 -0500

    Edited path on overdue button to point to correct page.

--- app/views/dashboard.blade.php
+++ app/views/dashboard.blade.php
@@ -21,20 +21,20 @@
                     <a class="tooltipped" data-position="top" data-tooltip="Add New Client" href="{{{action('ClientsController@create')}}}"><i class="z-depth-1 person-icon medium material-icons">person_add</i></a>
                 
                     <!-- due dates -->
-                     <a class="tooltipped" data-position="top" data-tooltip="View Due Dates" href="{{{action('ProjectsController@index')}}}"><i class="z-depth-1 cal-icon medium material-icons">today</i></a>
+                     <a class="tooltipped" data-position="top" data-tooltip="View Due Dates" href="#"><i class="z-depth-1 cal-icon medium material-icons">today</i></a>
                 </div>
             </div>
 
             <div class="row">
                 <div class="col s6 col offset-s6">
                     <!-- late projects -->
-                     <a class="tooltipped" data-position="bottom" data-tooltip="View Late Projects" href="{{{action('ProjectsController@index')}}}"><i class="z-depth-1 late-icon medium material-icons">assignment_late</i></a>
+                     <a class="tooltipped" data-position="bottom" data-tooltip="View Late Projects" href="{{{action('ProjectsController@showOverdue')}}}"><i class="z-depth-1 late-icon medium material-icons">assignment_late</i></a>
 
                     <!-- all contacts -->
                     <a class="tooltipped" data-position="bottom" data-tooltip="View All Clients" href="{{{action('ClientsController@index')}}}"><i class="z-depth-1 group-icon medium material-icons">group</i></a>
 
                     <!-- pay dates -->
-                    <a class="tooltipped" data-position="bottom" data-tooltip="View Pay Dates" href="{{{action('ProjectsController@index')}}}"><i class="z-depth-1 money-icon medium material-icons">monetization_on</i></a>
+                    <a class="tooltipped" data-position="bottom" data-tooltip="View Pay Dates" href="#"><i class="z-depth-1 money-icon medium material-icons">monetization_on</i></a>
                 </div>
                 <div class="col s6 col offset-s6">
 
