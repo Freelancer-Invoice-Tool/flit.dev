3d6ea20205f4913e2e59762d786f405a9c3f1360
Alan <alaurit@gmail.com>
2016-06-17 11:24:03 -0500

    further progress on index page - took out pagination on condensed tables

--- app/views/projects/index.blade.php
+++ app/views/projects/index.blade.php
@@ -16,7 +16,8 @@
 
                 <!-- expanded index visible on horizontal tablet and larger -->
                 <!-- <div class="col s3"> -->
-                    <table class="striped hide-on-med-and-down">
+                <div class="hide-on-med-and-down">
+                    <table class="striped">
                         <tr>
                             <th>Project</th>
                             <th>Client</th>
@@ -32,23 +33,43 @@
                                 <td><a href="{{{ action('ProjectsController@show', $project->id) }}}">{{{$project->name}}}</a></td>
                                 <td><a href="{{{ action('ClientsController@show', $project->client_id) }}}">{{{$project->client->client_name}}}</a></td>
                                 <td>{{{$project->due_date->format('m-d-Y')}}}</td>
+                                @if ((strpos($project->project_submitted_date, '-0001'))===false)
                                 <td>{{{$project->project_submitted_date->format('m-d-Y')}}}</td>
+                                @else
+                                <td> </td>
+                                @endif
+                                @if ((strpos($project->invoice_submitted_date, '-0001'))===false)
                                 <td>{{{$project->invoice_submitted_date->format('m-d-Y')}}}</td> 
+                                @else
+                                <td> </td>
+                                @endif
+                                @if ((strpos($project->invoice_approval_date, '-0001'))===false)
                                 <td>{{{$project->invoice_approval_date->format('m-d-Y')}}}</td>
+                                @else
+                                <td> </td>
+                                @endif
+                                @if ((strpos($project->pay_date, '-0001'))===false)
                                 <td>{{{$project->pay_date->format('m-d-Y')}}}</td>
+                                @else
+                                <td> </td>
+                                @endif
+                                @if ((strpos($project->payment_received, '-0001'))===false)
                                 <td>{{{$project->payment_received}}}</td>
+                                @else
+                                <td> </td>
+                                @endif
                             </tr>
                         @endforeach  
                     </table>
                     {{ $projects->links() }}
-                <!-- </div> -->
+                </div>
 
                 <!-- condensed index visible on vertical tablet and smaller -->
                 <!-- <div class="col s3"> -->
                 <div id="mobile-project-index" class="hide-on-large-only">
 
                     @if ($due_projects->count()>0)
-                    <h2>Projects Due</h2>
+                        <h2>Projects Due</h2>
                         <table class="striped">
                             <tr>
                                 <th>
@@ -69,10 +90,10 @@
                                
                             @endforeach
                         </table>
-                        {{$due_projects->links()}}
                     @endif
 
-                    <h2>Needs Invoice Issued</h2>
+                    @if($needs_invoice->count()>0)
+                        <h2>Needs Invoice Issued</h2>
                         <table class="striped">
                             <tr>
                                 <th>
@@ -82,23 +103,22 @@
                                 <th>Project Submitted</th>
                                 <th>Conditions</th>
                             </tr>
-                            @foreach($projects as $project)
-                                @if($project->project_submitted_date->format('m-d-Y')!=0 && $project->invoice_submitted_date->format('m-d-Y')==0)
+                            @foreach($needs_invoice as $uninvoiced_project)
                                     <tr>
                                         <td>
-                                            <p><a href="{{{ action('ProjectsController@show', $project->id) }}}">{{{$project->name}}}</a></p>
-                                            <p><a href="{{{ action('ClientsController@show', $project->client_id) }}}">{{{$project->client->client_name}}}</a></p>
+                                            <p><a href="{{{ action('ProjectsController@show', $project->id) }}}">{{{$uninvoiced_project->name}}}</a></p>
+                                            <p><a href="{{{ action('ClientsController@show', $project->client_id) }}}">{{{$uninvoiced_project->client->client_name}}}</a></p>
                                         </td>
-                                        <td>{{{$project->project_submitted_date->format('m-d-Y')}}}</td>
+                                        <td>{{{$uninvoiced_project->project_submitted_date->format('m-d-Y')}}}</td>
                                         <!-- verify payment conditions exist for client-->
-                                        <td>{{{Client::find($project->client_id)->payment_terms}}} {{{Client::find($project->client_id)->submission_or_approval}}}</td>
-                                        
+                                        <td>{{{Client::find($uninvoiced_project->client_id)->payment_terms}}} {{{Client::find($uninvoiced_project->client_id)->submission_or_approval}}}</td>
                                     </tr>
-                                @endif
                             @endforeach
                         </table>
+                    @endif
 
-                    <h2>Invoice Needs Approval</h2>
+                    @if($needs_approval->count()>0)
+                        <h2>Invoice Needs Approval</h2>
                         <table class="striped">
                             <tr>
                                 <th>
@@ -108,22 +128,22 @@
                                 <th>Invoice Submitted</th>
                                 <th>Conditions</th>
                             </tr>
-                            @foreach($projects as $project)
-                                @if($project->invoice_submitted_date->format('m-d-Y')!=0 && $project->invoice_approval_date->format('m-d-Y')==0)
-                                    <tr>
-                                        <td>
-                                            <p><a href="{{{ action('ProjectsController@show', $project->id) }}}">{{{$project->name}}}</a></p>
-                                            <p><a href="{{{ action('ClientsController@show', $project->client_id) }}}">{{{$project->client->client_name}}}</a></p>
-                                        </td>
-                                        <td>{{{$project->invoice_submitted_date->format('m-d-Y')}}}</td>
-                                        <td>{{{Client::find($project->client_id)->payment_terms}}} {{{Client::find($project->client_id)->submission_or_approval}}}</td>
-                                    </tr>
-                                @endif
+                            @foreach($needs_approval as $unapproved_project)
+                                <tr>
+                                    <td>
+                                        <p><a href="{{{ action('ProjectsController@show', $project->id) }}}">{{{$unapproved_project->name}}}</a></p>
+                                        <p><a href="{{{ action('ClientsController@show', $project->client_id) }}}">{{{$unapproved_project->client->client_name}}}</a></p>
+                                    </td>
+                                    <td>{{{$project->invoice_submitted_date->format('m-d-Y')}}}</td>
+                                    <td>{{{Client::find($unapproved_project->client_id)->payment_terms}}} {{{Client::find($unapproved_project->client_id)->submission_or_approval}}}</td>
+                                </tr>
                             @endforeach
                         </table>
+                    @endif
 
-                    <h2>Awaiting Payment</h2>
-                    <table class="striped">
+                    @if($awaiting_payment->count()>0)
+                        <h2>Awaiting Payment</h2>
+                        <table class="striped">
                             <tr>
                                 <th>
                                     <p>Project</p>
@@ -131,20 +151,22 @@
                                 </th>
                                 <th>Expected pay date</th>
                             </tr>
-                            @foreach($projects as $project)
-                                @if($project->invoice_approval_date->format('m-d-Y')!=0 && $project->payment_received==0)
+                            @foreach($awaiting_payment as $unpaid_project)
+                                
                                     <tr>
                                         <td>
-                                            <p><a href="{{{ action('ProjectsController@show', $project->id) }}}">{{{$project->name}}}</a></p>
-                                            <p><a href="{{{ action('ClientsController@show', $project->client_id) }}}">{{{$project->client->client_name}}}</a></p>
+                                            <p><a href="{{{ action('ProjectsController@show', $project->id) }}}">{{{$unpaid_project->name}}}</a></p>
+                                            <p><a href="{{{ action('ClientsController@show', $project->client_id) }}}">{{{$unpaid_project->client->client_name}}}</a></p>
                                         </td>
-                                        <td>{{{$project->pay_date->format('m-d-Y')}}}</td>
+                                        <td>{{{$unpaid_project->pay_date->format('m-d-Y')}}}</td>
                                     </tr>
-                                @endif
                             @endforeach
                         </table>
-                    <h2>Project Completed</h2>
-                    <table class="striped">
+                    @endif
+
+                    @if($project_completed->count()>0)
+                        <h2>Project Completed</h2>
+                        <table class="striped">
                             <tr>
                                 <th>
                                     <p>Project</p>
@@ -153,21 +175,18 @@
                                 <th>Expected pay date</th>
                                 <th>Actual pay date</th>
                             </tr>
-                            @foreach($projects as $project)
-                                @if($project->payment_received!=0)
-                                    <tr>
-                                        <td>
-                                            <p><a href="{{{ action('ProjectsController@show', $project->id) }}}">{{{$project->name}}}</a></p>
-                                            <p><a href="{{{ action('ClientsController@show', $project->client_id) }}}">{{{$project->client->client_name}}}</a></p>
-                                        </td>
-                                        <td>{{{$project->pay_date->format('m-d-Y')}}}</td>
-                                        <td>{{{$project->pay_received}}}</td>
-                                    </tr>
-                                @endif
+                            @foreach($project_completed as $completed_project)
+                                <tr>
+                                    <td>
+                                        <p><a href="{{{ action('ProjectsController@show', $project->id) }}}">{{{$completed_project->name}}}</a></p>
+                                        <p><a href="{{{ action('ClientsController@show', $project->client_id) }}}">{{{$completed_project->client->client_name}}}</a></p>
+                                    </td>
+                                    <td>{{{$completed_project->pay_date->format('m-d-Y')}}}</td>
+                                    <td>{{{$completed_project->payment_received}}}</td>
+                                </tr>
                             @endforeach
                         </table>
-
-                    {{ $projects->links() }}
+                    @endif
                 </div>
                 <!-- </div> -->
 
