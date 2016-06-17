6a4a9db45d93f5a0581fc915b487dc1b53e31fea
kcates81 <kriscates81@gmail.com>
2016-06-17 15:42:58 -0500

    made changes to parseDates function

--- app/helpers.php
+++ app/helpers.php
@@ -1,11 +1,12 @@
 <?php 
 function parseDates($dateString)
 {
+
     $possibleDate = new DateTime($dateString);
 
     $result = $possibleDate->format('Y-m-d');
 
-    if($result === '-0001-11-30'){
+    if($result === '-0001-11-30' || empty($result)){
 
         return null;
 
