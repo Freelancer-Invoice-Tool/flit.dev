2941e33e89273190510e2be38f45a171f06e030a
BobbieO <bobbie2504@msn.com>
2016-06-21 18:19:40 -0500

    Added link back to log in on sign up page, edited mobile view of signup page.

--- app/views/users/signup.blade.php
+++ app/views/users/signup.blade.php
@@ -11,7 +11,8 @@
     <div class="container">
         <div class="row">
             <div class="col s6">
-                <h1>Signup</h1>
+                <h2 class="hide-on-med-and-down">Sign Up</h2>
+                <h3 class="hide-on-large-only">Sign Up</h3>
             </div>
         </div>
 
@@ -43,10 +44,35 @@
                 </div>
             </div>
 
-            <div class="row">
-                <div class="col s6 left-align">
-                    <button type="submit" class="btn edit-btn btn-margin no-wrap">Create Account</button>
-                </div>     
+            <!-- expanded index visible on horizontal tablet and larger -->
+            <div class="row hide-on-med-and-down">
+                <div class="col s6 hide-on-med-and-down">
+                    <button type="submit" class="btn waves-effect waves-light edit-btn no-wrap">Create Account</button>
+                </div>
+                
+                <div class="col s6 hide-on-med-and-down">
+                    <p>Already have an account?</p>
+                    <a class="no-wrap" href="{{{action('HomeController@showHome')}}}">Log In</a>
+                </div>
+            </div>
+
+            <!-- condensed index visible on vertical tablet and smaller -->
+            <div class="row hide-on-large-only">
+                <div class="col s12 hide-on-large-only center-align">
+                    <button type="submit" class="btn waves-effect waves-light edit-btn no-wrap">Create Account</button>
+                </div>
+
+                <div class="mobile-login row hide-on-large-only">
+                    <div class="col s12">
+                        <p class="center-align">Already have an account?</p>
+                    </div>
+                </div>
+
+                <div class="row hide-on-large-only">
+                    <div class="col s12 center-align">
+                        <a class="no-wrap" href="{{{action('HomeController@showHome')}}}">Log In</a>
+                    </div>
+                </div>
             </div>
                     
         {{ Form::close() }}
