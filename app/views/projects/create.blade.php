efa0a69d73e2c0522fb184759525e79764a8cd22
kcates81 <kriscates81@gmail.com>
2016-06-21 17:50:41 -0500

    altered data types for all date fields

--- app/views/projects/create.blade.php
+++ app/views/projects/create.blade.php
@@ -1,144 +1,144 @@
 @extends('layouts.master')
 
 @section('title')
-    Flit: Add New Project
+    FLIT: Add New Project
 @stop
 
 @section('content')
 <main>
-    <div class="container"> 
+<div class="container"> 
+    <div class="row">
+        <div>
+            <h2>Add New Project</h2>
+        </div>
+    </div>
+
+    <div>
+        {{ Form::open(array('action'=>'ProjectsController@store', 'class' => 'col s8 box', 'enctype' => 'multipart/form-data')) }}
+            
         <div class="row">
-            <div>
-                <h2>Add New Project</h2>
+            <div class="input-field col s12">
+                {{ Form::text('name', null, array('id'=>'name')) }}
+                {{ Form::label('name', 'Project Name') }}
+            </div>
+            <div class="input-field col s12">
+                <input type="date" class="datepicker" id="due_date" name="due_date">
+                {{ Form::label('due_date', 'Due Date') }}
+            </div>
+        </div>
+    
+        <div class="row">
+            <div class="input-field col s12">
+                <select id="client_dropdown" name="client_dropdown">
+                    <option label='select' selected>Select from dropdown</option>
+                    <option data-clientid='create' label='create_new'>Create new</option>
+                    @foreach($clients as $client)
+                        <option data-clientid="{{{ $client->id }}}" label="{{{ $client->client_name }}}">{{{ $client->client_name }}}</option>
+                    @endforeach
+                </select>
+                <label>Select client</label>
             </div>
         </div>
 
-        <div>
-            {{ Form::open(array('action'=>'ProjectsController@store', 'class' => 'col s8 box', 'enctype' => 'multipart/form-data')) }}
-                
-            <div class="row">
-                <div class="input-field col s12">
-                    {{ Form::text('name', null, array('id'=>'name')) }}
-                    {{ Form::label('name', 'Project Name') }}
+        <!-- create client fields should remain hidden unless user selects to create new -->
+        <div id="create_client" class='hide'>
+            <div class='row col s12'>
+                {{Form::label('client_name', 'Client Name')}} 
+                {{Form::text('client_name', Input::old('title'), array('class' => 'form-control other-class another', 'placeholder' => 'e.g. ABC Company, Inc.'))}} 
+            </div>
+            <div class='row'>
+                <div class='col s12'>
+                    {{Form::label('payment_terms', 'Payment Terms (in number of days)')}} 
+                    {{Form::number('payment_terms', Input::old('payment_terms'), array('class' => 'form-control other-class another', 'placeholder' => 'e.g. 30'))}} 
                 </div>
-                <div class="input-field col s12">
-                    <input type="date" class="datepicker" id="due_date" name="due_date">
-                    {{ Form::label('due_date', 'Due Date') }}
+                <div class='col s12'>
+                    {{Form::label('submission_or_approval', 'Does Payment Term start on invoice approval or invoice submission?')}} 
+                    {{Form::text('submission_or_approval', Input::old('submission_or_approval'), array('class' => 'form-control other-class another', 'placeholder' => 'e.g. submission'))}} 
                 </div>
+            </div>
+            <div class='row col s12'>
+                {{Form::label('main_poc_name', 'Main Point of Contact Name')}}
+                {{Form::text('main_poc_name', Input::old('main_poc_name'), array('class' => 'form-control other-class another', 'placeholder' => 'e.g. Joe Blow'))}}
+            </div>
+            <div class='row col s12'>
+                {{Form::label('main_poc_email', 'Main Point of Contact Email')}}
+                {{Form::text('main_poc_email', Input::old('main_poc_email'), array('class' => 'form-control other-class another', 'placeholder' => 'e.g. jblow@example.com'))}}
+            </div>
+            <div class='row col s12'>
+                {{Form::label('main_poc_phone', 'Main Point of Contact Phone')}}
+                {{Form::text('main_poc_phone', Input::old('main_poc_phone'), array('class' => 'form-control other-class another', 'placeholder' => 'e.g. 210-867-5309'))}}
+            </div>
+            <div class='row col s12'>
+                {{Form::label('main_poc_address', 'Main Point of Contact Address')}}
+                {{Form::text('main_poc_address', Input::old('main_poc_address'), array('class' => 'form-control other-class another', 'placeholder' => 'e.g. 123 Some Street Anytown, TX 78253'))}}
+            </div>
+
+        </div>
+        <!-- end of client creation fields -->
+
+            <div class="input-field col s12">
+                {{Form::hidden('project_status', 'started', array('id'=>'project_status'))}}   
+            </div>
+        <div class="row">
+            <div class="input-field col s6">
+                <input type="date" class="datepicker" id="project_submitted_date" name="project_submitted_date">
+                {{ Form::label('project_submitted_date', 'Date Project Submitted') }}
+            </div>
+        </div>
+        <div class="row">
+            <div class="input-field col s6">
+                <input type="date" class="datepicker" id="invoice_submitted_date" name="invoice_submitted_date">
+                {{ Form::label('invoice_submitted_date', 'Date Invoice Submitted') }}
             </div>
         
-            <div class="row">
-                <div class="input-field col s12">
-                    <select id="client_dropdown" name="client_dropdown">
-                        <option label='select' selected>Select from dropdown</option>
-                        <option data-clientid='create' label='create_new'>Create new</option>
-                        @foreach($clients as $client)
-                            <option data-clientid="{{{ $client->id }}}" label="{{{ $client->client_name }}}">{{{ $client->client_name }}}</option>
-                        @endforeach
-                    </select>
-                    <label>Select client</label>
-                </div>
+            <div class="input-field col s6">
+                <input type="date" class="datepicker" id="invoice_approval_date" name="invoice_approval_date">
+                {{ Form::label('invoice_approval_date', 'Date Invoice Approved') }}
             </div>
-
-            <!-- create client fields should remain hidden unless user selects to create new -->
-            <div id="create_client" class='hide'>
-                <div class='row col s12'>
-                    {{Form::label('client_name', 'Client Name')}} 
-                    {{Form::text('client_name', Input::old('title'), array('class' => 'form-control other-class another', 'placeholder' => 'e.g. ABC Company, Inc.'))}} 
-                </div>
-                <div class='row'>
-                    <div class='col s12'>
-                        {{Form::label('payment_terms', 'Payment Terms (in number of days)')}} 
-                        {{Form::number('payment_terms', Input::old('payment_terms'), array('class' => 'form-control other-class another', 'placeholder' => 'e.g. 30'))}} 
-                    </div>
-                    <div class='col s12'>
-                        {{Form::label('submission_or_approval', 'Does Payment Term start on invoice approval or invoice submission?')}} 
-                        {{Form::text('submission_or_approval', Input::old('submission_or_approval'), array('class' => 'form-control other-class another', 'placeholder' => 'e.g. submission'))}} 
-                    </div>
-                </div>
-                <div class='row col s12'>
-                    {{Form::label('main_poc_name', 'Main Point of Contact Name')}}
-                    {{Form::text('main_poc_name', Input::old('main_poc_name'), array('class' => 'form-control other-class another', 'placeholder' => 'e.g. Joe Blow'))}}
-                </div>
-                <div class='row col s12'>
-                    {{Form::label('main_poc_email', 'Main Point of Contact Email')}}
-                    {{Form::text('main_poc_email', Input::old('main_poc_email'), array('class' => 'form-control other-class another', 'placeholder' => 'e.g. jblow@example.com'))}}
-                </div>
-                <div class='row col s12'>
-                    {{Form::label('main_poc_phone', 'Main Point of Contact Phone')}}
-                    {{Form::text('main_poc_phone', Input::old('main_poc_phone'), array('class' => 'form-control other-class another', 'placeholder' => 'e.g. 210-867-5309'))}}
-                </div>
-                <div class='row col s12'>
-                    {{Form::label('main_poc_address', 'Main Point of Contact Address')}}
-                    {{Form::text('main_poc_address', Input::old('main_poc_address'), array('class' => 'form-control other-class another', 'placeholder' => 'e.g. 123 Some Street Anytown, TX 78253'))}}
-                </div>
-
-            </div>
-            <!-- end of client creation fields -->
-
-                <div class="input-field col s12">
-                    {{Form::hidden('project_status', 'started', array('id'=>'project_status'))}}   
-                </div>
-            <div class="row">
-                <div class="input-field col s6">
-                    <input type="date" class="datepicker" id="project_submitted_date" name="project_submitted_date">
-                    {{ Form::label('project_submitted_date', 'Date Project Submitted') }}
-                </div>
-            </div>
-            <div class="row">
-                <div class="input-field col s6">
-                    <input type="date" class="datepicker" id="invoice_submitted_date" name="invoice_submitted_date">
-                    {{ Form::label('invoice_submitted_date', 'Date Invoice Submitted') }}
-                </div>
-            
-                <div class="input-field col s6">
-                    <input type="date" class="datepicker" id="invoice_approval_date" name="invoice_approval_date">
-                    {{ Form::label('invoice_approval_date', 'Date Invoice Approved') }}
-                </div>
-            </div>
-            <div class="row">
-                <div class="input-field col s6">
-                    <input type="date" class="datepicker" id="pay_date" name="pay_date">
-                    {{ Form::label('pay_date', 'Exp Pay Date') }}
-                </div>
-            
-                <div class="input-field col s6">
-                    <input type="date" class="datepicker" id="payment_received" name="payment_received">
-                    {{ Form::label('payment_received', 'Date Payment Received') }}
-                </div>
-            </div>
-            <div class="row">
-                <div class="input-field col s6">
-                    {{ Form::label('project_poc_name', 'Project Contact\'s Name') }}
-                    {{ Form::text('project_poc_name', null, array('id'=>'project_poc_name')) }}
-                </div>
-           
-                <div class="input-field col s6">
-                    {{ Form::label('project_poc_phone', 'Project Contact\'s Phone') }}
-                    {{ Form::text('project_poc_phone', null, array('id'=>'project_poc_phone')) }}
-                </div>
-            </div>
-            <div class="row">
-                <div class="input-field col s12">
-                    {{ Form::label('project_poc_email', 'Project Contact\'s Email') }}
-                    {{ Form::text('project_poc_email', null, array('id'=>'project_poc_email')) }}
-                </div>
-            </div>
-            <div class="row">
-                <div class="input-field col s12">
-                    {{ Form::label('project_poc_address', 'Project Contact\'s Address') }}
-                    {{ Form::text('project_poc_address', null, array('id'=>'project_poc_address')) }}
-                </div>
-            </div>
- 
-            <div class="row center-align">
-                <div class="col s12">
-                    <button type="submit" class="btn waves-effect waves-light edit-btn">Add New Project</button>
-                </div>
-            </div> 
         </div>
-        {{ Form::close() }}    
+        <div class="row">
+            <div class="input-field col s6">
+                <input type="date" class="datepicker" id="pay_date" name="pay_date">
+                {{ Form::label('pay_date', 'Exp Pay Date') }}
+            </div>
+        
+            <div class="input-field col s6">
+                <input type="date" class="datepicker" id="payment_received" name="payment_received">
+                {{ Form::label('payment_received', 'Date Payment Received') }}
+            </div>
+        </div>
+        <div class="row">
+            <div class="input-field col s6">
+                {{ Form::label('project_poc_name', 'Project Contact\'s Name') }}
+                {{ Form::text('project_poc_name', null, array('id'=>'project_poc_name')) }}
+            </div>
+       
+            <div class="input-field col s6">
+                {{ Form::label('project_poc_phone', 'Project Contact\'s Phone') }}
+                {{ Form::text('project_poc_phone', null, array('id'=>'project_poc_phone')) }}
+            </div>
+        </div>
+        <div class="row">
+            <div class="input-field col s12">
+                {{ Form::label('project_poc_email', 'Project Contact\'s Email') }}
+                {{ Form::text('project_poc_email', null, array('id'=>'project_poc_email')) }}
+            </div>
+        </div>
+        <div class="row">
+            <div class="input-field col s12">
+                {{ Form::label('project_poc_address', 'Project Contact\'s Address') }}
+                {{ Form::text('project_poc_address', null, array('id'=>'project_poc_address')) }}
+            </div>
+        </div>
+
+        <div class="row center-align">
+            <div class="col s12">
+                <button type="submit" class="btn waves-effect waves-light edit-btn">Add New Project</button>
+            </div>
+        </div> 
     </div>
+    {{ Form::close() }}    
+</div>
 </main>
 @stop
 
@@ -201,10 +201,6 @@
                 $('#main_poc_address').val("");
             }
 
-        });
-
-
-
-            
+        });    
     </script>
 @stop    
--- app/views/projects/overdue.blade.php
+++ app/views/projects/overdue.blade.php
@@ -1,72 +1,72 @@
 @extends('layouts.master')
 
 @section('title')
-    Flit: Overdue Projects
+    FLIT: Overdue Projects
 @stop
 
 @section('content')
 <main>
-    <div class="container">
+<div class="container">
 
-        <div>
-            <h1>Overdue Projects</h1>
-        </div>
+    <div>
+        <h1>Overdue Projects</h1>
+    </div>
 
-        <!-- expanded index visible on horizontal tablet and larger -->
-        <div class="hide-on-med-and-down">
-            <table class="striped">
+    <!-- expanded index visible on horizontal tablet and larger -->
+    <div class="hide-on-med-and-down">
+        <table class="striped">
+            <tr>
+                <th>Project Due Date</th>
+                <th>Project</th>
+                <th>Project Status</th>
+                <th>Client</th>
+                <th>Project Point of Contact Name</th>
+                <th>Project Point of Contact Email</th>
+                <th>Project Description</th>
+            </tr>
+            @foreach($projects as $project)
                 <tr>
-                    <th>Project Due Date</th>
-                    <th>Project</th>
-                    <th>Project Status</th>
-                    <th>Client</th>
-                    <th>Project Point of Contact Name</th>
-                    <th>Project Point of Contact Email</th>
-                    <th>Project Description</th>
-                </tr>
-                @foreach($projects as $project)
-                    <tr>
-                        <td>{{{$project->due_date->format('m-d-Y')}}}</td>
+                    <td>{{{$project->due_date->format('m-d-Y')}}}</td>
 
-                        <td><a href="{{{ action('ProjectsController@show', $project->id) }}}">{{{$project->name}}}</a></td>
+                    <td><a href="{{{ action('ProjectsController@show', $project->id) }}}">{{{$project->name}}}</a></td>
 
-                        <td>{{{$project->project_status}}}</td>
+                    <td>{{{$project->project_status}}}</td>
 
-                        <td><a href="{{{ action('ClientsController@show', $project->client_id) }}}">{{{$project->client->client_name}}}</a></td>
-
-                        <td>{{{$project->project_poc_name}}}</td>
-                        <td>{{{$project->project_poc_email}}}</td>
-                        <td>{{{$project->description}}}</td>
-
-                    </tr>
-                @endforeach  
-            </table>
-            {{ $projects->links() }}
-        </div>
-
-        <!-- condensed index visible on vertical tablet and smaller -->
-        <div id="mobile-project-index" class="hide-on-large-only">
-            <table class="striped">
-                <tr>
-                    <th>
-                        <p>Project</p>
-                        <p>Project Due Date</p>
-                    </th>
-                    <th>Client</th>
-                </tr>
-
-                @foreach($projects as $project)    
-                <tr>
-                    <td>
-                        <p><a href="{{{ action('ProjectsController@show', $project->id) }}}">{{{$project->name}}}</a></p>
-                        <p>{{{$project->due_date->format('m-d-Y')}}}</p>
-                    </td>
                     <td><a href="{{{ action('ClientsController@show', $project->client_id) }}}">{{{$project->client->client_name}}}</a></td>
-                </tr>
-                @endforeach
-            </table>
-        </div>        
 
-    </div> <!-- closes container -->      
+                    <td>{{{$project->project_poc_name}}}</td>
+                    <td>{{{$project->project_poc_email}}}</td>
+                    <td>{{{$project->description}}}</td>
+
+                </tr>
+            @endforeach  
+        </table>
+        {{ $projects->links() }}
+    </div>
+
+    <!-- condensed index visible on vertical tablet and smaller -->
+    <div id="mobile-project-index" class="hide-on-large-only">
+        <table class="striped">
+            <tr>
+                <th>
+                    <p>Project</p>
+                    <p>Project Due Date</p>
+                </th>
+                <th>Client</th>
+            </tr>
+
+            @foreach($projects as $project)    
+            <tr>
+                <td>
+                    <p><a href="{{{ action('ProjectsController@show', $project->id) }}}">{{{$project->name}}}</a></p>
+                    <p>{{{$project->due_date->format('m-d-Y')}}}</p>
+                </td>
+                <td><a href="{{{ action('ClientsController@show', $project->client_id) }}}">{{{$project->client->client_name}}}</a></td>
+            </tr>
+            @endforeach
+        </table>
+    </div>        
+
+</div> <!-- closes container -->      
 </main>
 @stop
