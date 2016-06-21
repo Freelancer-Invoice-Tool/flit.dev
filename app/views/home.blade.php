7ec0c8afcd05c6cc9260f5bdaa248fa849a2870a
BobbieO <bobbie2504@msn.com>
2016-06-21 09:58:28 -0500

    Edited log in page for mobile view, added css class available for all buttons for mobile view.

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -36,17 +36,38 @@
                             <label for="password">Password</label>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col s6">
+
+                    <!-- expanded index visible on horizontal tablet and larger -->
+                    <div class="row hide-on-med-and-down">
+                        <div class="col s6 hide-on-med-and-down">
                             <button type="submit" class="btn waves-effect waves-light edit-btn">Log In</button>
                         </div>
                         
-                        <div class="col s6">
+                        <div class="col s6 hide-on-med-and-down">
                             <p>Don't have an account?</p>
                             <a href="{{{action('HomeController@showSignup')}}}">Sign Up</a>
                         </div>
-                {{Form::close()}}
                     </div>
+
+                    <!-- condensed index visible on vertical tablet and smaller -->
+                    <div class="row hide-on-large-only">
+                        <div class="col s6 hide-on-large-only">
+                            <button type="submit" class="btn waves-effect waves-light edit-btn no-wrap">Log In</button>
+                        </div>
+
+                        <div class="mobile-login row hide-on-large-only">
+                            <div class="col s12">
+                                <p>Don't have an account?</p>
+                            </div>
+                        </div>
+
+                        <div class="row hide-on-large-only">
+                            <div class="col s6">
+                                <a href="{{{action('HomeController@showSignup')}}}">Sign Up</a>
+                            </div>
+                        </div>
+                    </div>
+                {{Form::close()}}
             </div>
         </div>
     </div>
