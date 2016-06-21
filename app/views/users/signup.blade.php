c580ed0bc022dc0cf853b1456aaab86e9ddeacad
BobbieO <bobbie2504@msn.com>
2016-06-21 13:51:27 -0500

    Added rows, cols, div, classes for consistency with other pages.

--- app/views/users/signup.blade.php
+++ app/views/users/signup.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 
 @section('title')
-    Flit Home Login
+    FLIT Home Login
 @stop
 
 @section('content')
@@ -9,48 +9,48 @@
 <main>
 
     <div class="container">
-        {{ Form::open(array('action'=>'UserController@store')) }}
-            <div class="row">
-                <div class="col s8">
-                    <h1>Signup for Flit!</h1>
-                </div>
-            </div>
-
+        <div class="row">
             <div class="col s6">
-                 <div class="row">
-                    <form class="col s12">
-                        <div class="row">
-                            <div class="input-field col s6">
-                                {{Form::text('first_name', null, array('class'=>'validate'))}}
-                                {{ Form::label('first_name', 'First Name') }}
-                            </div>
-                            <div class="input-field col s6">
-                                {{ Form::text('last_name', null, array('class'=>'validate')) }}
-                                {{ Form::label('last_name', 'Last Name') }}
-                            </div>
-                        </div>
+                <h1>Signup</h1>
+            </div>
+        </div>
 
-                        <div class="row">
-                            <div class="input-field col s12">
-                                {{ Form::email('email', null, array('class'=>'validate')) }}
-                                {{ Form::label('email', 'Email') }}
-                            </div>
-                        </div>
-
-                        <div class="row">
-                            <div class="input-field col s12">
-                                {{ Form::password('password', null, array('class'=>'validate')) }}
-                                {{ Form::label('password', 'Password') }}
-                            </div>
-                        </div>
-
-                        <div>
-                            <button type="submit" class="btn">Create Account</button>     
-                        </div>
-                    </form>
+        <div>
+        {{ Form::open(array('action'=>'UserController@store')) }}
+                      
+            <div class="row">
+                <div class="input-field col s6">
+                    {{Form::text('first_name', null, array('class'=>'validate'))}}
+                    {{ Form::label('first_name', 'First Name') }}
+                </div>
+                <div class="input-field col s6">
+                    {{ Form::text('last_name', null, array('class'=>'validate')) }}
+                    {{ Form::label('last_name', 'Last Name') }}
                 </div>
             </div>
+
+            <div class="row">
+                <div class="input-field col s12">
+                    {{ Form::email('email', null, array('class'=>'validate')) }}
+                    {{ Form::label('email', 'Email') }}
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="input-field col s12">
+                    {{ Form::password('password', null, array('class'=>'validate')) }}
+                    {{ Form::label('password', 'Password') }}
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col s6 left-align">
+                    <button type="submit" class="btn edit-btn btn-margin no-wrap">Create Account</button>
+                </div>     
+            </div>
+                    
         {{ Form::close() }}
+        </div>
     </div>
 </main>
 @stop
