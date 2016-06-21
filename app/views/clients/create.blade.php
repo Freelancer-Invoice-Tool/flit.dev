ac31a243e523a13057317149ff71ed8310e25f2f
BobbieO <bobbie2504@msn.com>
2016-06-21 12:41:34 -0500

    Added input-field class to form, added class to alert message.

--- app/views/clients/create.blade.php
+++ app/views/clients/create.blade.php
@@ -14,7 +14,7 @@
         </div> 
 
         <div class="row">
-            <div class="col s10">
+            <div class="col s10 alert-danger">
                 <div id="errors">
                     <h3>{{ $errors->first('client_name', '<span class="help-block">:message</span>') }}</h3> 
                 </div> 
@@ -23,14 +23,14 @@
 
         {{Form::open(array('action' => 'ClientsController@store'))}} 
             <div class="row">
-                <div class="col s10">
+                <div class="col s10 input-field">
                     {{Form::label('client_name', 'Client Name')}} 
                     {{Form::text('client_name', Input::old('title'), array('class' => 'form-control other-class another', 'placeholder' => 'e.g. ABC Company, Inc.'))}}
                 </div> 
             </div>
 
             <div class="row">
-                <div class="col s10">
+                <div class="col s10 input-field">
                     {{Form::label('payment_terms', 'Payment Terms (in number of days)')}} 
                     {{Form::number('payment_terms', Input::old('payment_terms'), array('class' => 'form-control other-class another', 'placeholder' => 'e.g. 30'))}} 
                 </div>
@@ -38,35 +38,35 @@
 
 
             <div class="row">
-                <div class="col s10">
+                <div class="col s10 input-field">
                     {{Form::label('submission_or_approval', 'Does Payment Term start on invoice approval or invoice submission?')}} 
                     {{Form::text('submission_or_approval', Input::old('submission_or_approval'), array('class' => 'form-control other-class another', 'placeholder' => 'e.g. submission'))}} 
                 </div>
             </div>
 
             <div class="row">
-                <div class="col s10">
+                <div class="col s10 input-field">
                     {{Form::label('main_poc_name', 'Main Point of Contact Name')}}
                     {{Form::text('main_poc_name', Input::old('main_poc_name'), array('class' => 'form-control other-class another', 'placeholder' => 'e.g. Joe Blow'))}}
                 </div>
             </div>
 
             <div class="row">
-                <div class="col s10">
+                <div class="col s10 input-field">
                     {{Form::label('main_poc_email', 'Main Point of Contact Email')}}
                     {{Form::text('main_poc_email', Input::old('main_poc_email'), array('class' => 'form-control other-class another', 'placeholder' => 'e.g. jblow@example.com'))}}
                 </div>
             </div>
 
             <div class="row">
-                <div class="col s10">
+                <div class="col s10 input-field">
                     {{Form::label('main_poc_phone', 'Main Point of Contact Phone')}}
                     {{Form::text('main_poc_phone', Input::old('main_poc_phone'), array('class' => 'form-control other-class another', 'placeholder' => 'e.g. 210-867-5309'))}}
                 </div>
             </div>
 
             <div class="row">
-                <div class="col s10">
+                <div class="col s10 input-field">
                     {{Form::label('main_poc_address', 'Main Point of Contact Address')}}
                     {{Form::text('main_poc_address', Input::old('main_poc_address'), array('class' => 'form-control other-class another', 'placeholder' => 'e.g. 123 Some Street Anytown, TX 78253'))}}
                 </div>
