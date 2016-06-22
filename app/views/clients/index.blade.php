44c791c4895c1a7ce48e12c6283245170e09f5c7
BobbieO <bobbie2504@msn.com>
2016-06-22 15:39:42 -0500

    Adjusted table view on all clients page for site consistency, added sections.

--- app/views/clients/index.blade.php
+++ app/views/clients/index.blade.php
@@ -6,68 +6,72 @@
 
 @section('content')
 <main>
-
     <div class="container">
-        <div>
-            <h2 class="hide-on-med-and-down">All Clients</h2>
-            <h3 class="hide-on-large-only">All Clients</h3>
-        </div>
-        <div class="right-align btn-margin">
-            <a class="waves-effect waves-light btn edit-btn" href="{{{action('ClientsController@create')}}}">Create New Client</a>
+        <div class="section">
+            <div>
+                <h2 class="hide-on-med-and-down">All Clients</h2>
+                <h3 class="hide-on-large-only">All Clients</h3>
+            </div>
+            <div class="right-align btn-margin">
+                <a class="waves-effect waves-light btn edit-btn" href="{{{action('ClientsController@create')}}}">Create New Client</a>
+            </div>
         </div>
 
         <!-- expanded index visible on horizontal tablet and larger -->
-        <div class="hide-on-med-and-down">
-            <table class="striped">
-                <thead>
-                    <tr>
-                        <th>Client Name</th>
-                        <th>Main POC Name</th>
-                        <th>Main POC Phone Number</th>
-                        <th>Main POC Email</th>
-                        <th>Main POC Address</th>
-                    </tr>
-                </thead>  
-                <tbody>
-                    @foreach($clients as $client)
+        <div class="section">
+            <div class="hide-on-med-and-down">
+                <table class="striped centered">
+                    <thead>
                         <tr>
-                             <td><a href="{{{ action('ClientsController@show', $client->id) }}}">{{{$client->client_name}}}</a></td>
-                             <td>{{{$client->main_poc_name}}}</td>
-                             <td>{{{$client->main_poc_phone}}}</td>
-                             <td><a href="mailto:{{{$client->main_poc_email}}}"></a>{{{$client->main_poc_email}}}</td>
-                             <td>{{{$client->main_poc_address}}}</td>
+                            <th class="center-align">Name</th>
+                            <th class="center-align">Main Contact</th>
+                            <th class="center-align">Contact Phone</th>
+                            <th class="center-align">Contact Email</th>
+                            <th class="center-align">Contact Address</th>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>   
+                    </thead>  
+                    <tbody>
+                        @foreach($clients as $client)
+                            <tr>
+                                 <td><a href="{{{ action('ClientsController@show', $client->id) }}}">{{{$client->client_name}}}</a></td>
+                                 <td>{{{$client->main_poc_name}}}</td>
+                                 <td>{{{$client->main_poc_phone}}}</td>
+                                 <td><a href="mailto:{{{$client->main_poc_email}}}"></a>{{{$client->main_poc_email}}}</td>
+                                 <td>{{{$client->main_poc_address}}}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>   
+            </div>
         </div>
 
         <!-- condensed index visible on vertical tablet and smaller -->
-        <div id="mobile-project-index" class="hide-on-large-only">
-            <table class="striped">
-                <thead>
-                    <tr>
-                        <th>Client Name</th>
-                        <th>Main POC Email</th>
-                    </tr>
-                </thead>  
-                <tbody>
-                    @foreach($clients as $client)
+        <div class="section">
+            <div id="mobile-project-index" class="hide-on-large-only">
+                <table class="striped centered">
+                    <thead>
                         <tr>
-                             <td><a href="{{{ action('ClientsController@show', $client->id) }}}">{{{$client->client_name}}}</a></td>
-                             <td><a href="mailto:{{{$client->main_poc_email}}}"></a>{{{$client->main_poc_email}}}</td>
+                            <th class="center-align">Name</th>
+                            <th class="center-align">Main POC Email</th>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>       
-        </div>
-        
-              
+                    </thead>  
+                    <tbody>
+                        @foreach($clients as $client)
+                            <tr>
+                                 <td><a href="{{{ action('ClientsController@show', $client->id) }}}">{{{$client->client_name}}}</a></td>
+                                 <td><a href="mailto:{{{$client->main_poc_email}}}"></a>{{{$client->main_poc_email}}}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>       
+            </div>
+        </div>  
+               
+        <div class="section">
+            <ul class="pagination center-align">
+                {{ $paginator->render() }}
+            </ul>    
+        </div>  
     </div> <!-- closes container -->
-    <div class="container">
-        <ul class="pagination center-align">
-            {{ $paginator->render() }}
-        </ul>    
-    </div>  
 </main>
 @stop
