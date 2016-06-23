9119b09683668dfcce8d0603a23d6e33ec358ce5
BobbieO <bobbie2504@msn.com>
2016-06-23 10:51:22 -0500

    Added input-field to divs on create project page.

--- app/views/projects/create.blade.php
+++ app/views/projects/create.blade.php
@@ -48,41 +48,41 @@
         <!-- create client fields should remain hidden unless user selects to create new -->
         <div id="create_client" class="hide">
             <div class="row">
-                <div class="col s12">
+                <div class="input-field col s12">
                     {{Form::label('client_name', 'Client Name')}} 
                     {{Form::text('client_name', Input::old('title'), array('class' => 'form-control other-class another', 'placeholder' => 'e.g. ABC Company, Inc.'))}}
                 </div> 
             </div>
 
             <div class="row">
-                <div class="col s6">
+                <div class="input-field col s6">
                     {{Form::label('payment_terms', 'Payment Terms (in number of days)')}} 
                     {{Form::number('payment_terms', Input::old('payment_terms'), array('class' => 'form-control other-class another', 'placeholder' => 'e.g. 30'))}} 
                 </div>
-                <div class="col s6">
+                <div class="input-field col s6">
                     {{Form::label('submission_or_approval', 'Does Payment Term start on invoice approval or invoice submission?')}} 
                     {{Form::text('submission_or_approval', Input::old('submission_or_approval'), array('class' => 'form-control other-class another', 'placeholder' => 'e.g. submission'))}} 
                 </div>
             </div>
 
             <div class="row">
-                <div class="col s6">
+                <div class="input-field col s6">
                     {{Form::label('main_poc_name', 'Main Contact Name')}}
                     {{Form::text('main_poc_name', Input::old('main_poc_name'), array('class' => 'form-control other-class another', 'placeholder' => 'e.g. Joe Blow'))}}
                 </div>
-                <div class="col s6">
+                <div class="input-field col s6">
                     {{Form::label('main_poc_email', 'Main Contact Email')}}
                     {{Form::text('main_poc_email', Input::old('main_poc_email'), array('class' => 'form-control other-class another', 'placeholder' => 'e.g. jblow@example.com'))}}
                 </div>
             </div>
 
             <div class="row">
-                <div class="col s6">
+                <div class="input-field col s6">
                     {{Form::label('main_poc_phone', 'Main Contact Phone')}}
                 {{Form::text('main_poc_phone', Input::old('main_poc_phone'), array('class' => 'form-control other-class another', 'placeholder' => 'e.g. 210-867-5309'))}}
                 </div>
 
-                <div class="col s6">
+                <div class="input-field col s6">
                     {{Form::label('main_poc_address', 'Main Contact Address')}}
                     {{Form::text('main_poc_address', Input::old('main_poc_address'), array('class' => 'form-control other-class another', 'placeholder' => 'e.g. 123 Some Street Anytown, TX 78253'))}}
                 </div>
